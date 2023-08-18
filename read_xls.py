ccc2fcb74d896987579bfaaaff77394cdc2e9f54
Lin-Xing Zeng <jasonphysics@outlook.com>
2023-08-18 21:52:43 +0800

    This version tried to add an experimental feature, change the sizes of the unit cells(still developing).

--- read_xls.py
+++ read_xls.py
@@ -1,5 +1,38 @@
 import xlrd,xlwt
 
+def ab_order(num):
+    count=1;comp=25
+    while num>comp:
+        count+=1;comp+=26**count
+    re='';n=num-comp+26**count;c=0
+    while n!=0:
+        div=n//26;mod=n%26
+        re=chr(ord('A')+mod-1 if c==0 else ord('A')+mod)+re
+        n=div;c+=1
+    return 'A'*(count-len(re))+re
+
+def manage_size(sheet,size,n1,n2):
+    if size==None:
+        return
+    if size[0]!=None:
+        for i in range(n2):
+            if isinstance(size[0],(tuple,list)):
+                if size[0][i]==None:
+                    continue
+                else:
+                    sheet.row_dimensions[i].height=size[0][i]
+            else:
+                sheet.row_dimensions[i].height=size[0]
+    if size[1]!=None:
+        for i in range(n1):
+            if isinstance(size[1],(tuple,list)):
+                if size[1][i]==None:
+                    continue
+                else:
+                    sheet.column_dimensions[ab_order[i]].width=size[1][i]
+            else:
+                sheet.column_dimensions[ab_order[i]].width=size[1]
+
 '''
 data in the .xlsx is recorded in a dictionary.
 structure of dictionary------------------------------------------------
@@ -41,7 +74,7 @@ def read_xls(file,container=list,by_row=True):
                                for j in range(ncols))
     return d
 
-def write_xls(d,output,encoding='utf-8',style_compression=0,by_row=True):
+def write_xls(d,output,encoding='utf-8',style_compression=0,by_row=True,size=None):
     workbook=xlwt.Workbook(encoding=encoding,style_compression=style_compression)
     ns=len(d.keys())
     if by_row in (True,False):
@@ -57,6 +90,15 @@ def write_xls(d,output,encoding='utf-8',style_compression=0,by_row=True):
                 n2=len(d[keys[k]][i])
                 for j in range(n2):
                     sheets[-1].write(i,j,d[keys[k]][i][j])
+            # manage size
+            if size == None:
+                continue
+            elif isinstance(size,(tuple,list)):
+                if len(size)==2:
+                    manage_size(sheets[-1],size,n1,max(len(j) for j in d[keys[k]]))
+            else:
+                if sheet in size.keys():
+                    manage_size(sheets[-1],size[sheet],n1,max(len(j) for j in d[keys[k]]))
         else:
             sheets.append(workbook.add_sheet(keys[k],cell_overwrite_ok=True))
             n1=len(d[keys[k]])
@@ -64,4 +106,13 @@ def write_xls(d,output,encoding='utf-8',style_compression=0,by_row=True):
                 n2=len(d[keys[k]][i])
                 for j in range(n2):
                     sheets[-1].write(j,i,d[keys[k]][i][j])
+            # manage size
+            if size == None:
+                continue
+            elif isinstance(size,(tuple,list)):
+                if len(size)==2:
+                    manage_size(sheets[-1],size,max(len(j) for j in d[keys[k]]),n1)
+            else:
+                if sheet in size.keys():
+                    manage_size(sheets[-1],size[sheet],max(len(j) for j in d[keys[k]]),n1)
     workbook.save(output)
--- read_xlsx.py
+++ read_xlsx.py
@@ -1,5 +1,38 @@
 import openpyxl
 
+def ab_order(num):
+    count=1;comp=25
+    while num>comp:
+        count+=1;comp+=26**count
+    re='';n=num-comp+26**count;c=0
+    while n!=0:
+        div=n//26;mod=n%26
+        re=chr(ord('A')+mod-1 if c==0 else ord('A')+mod)+re
+        n=div;c+=1
+    return 'A'*(count-len(re))+re
+
+def manage_size(sheet,size,n1,n2):
+    if size==None:
+        return
+    if size[0]!=None:
+        for i in range(n2):
+            if isinstance(size[0],(tuple,list)):
+                if size[0][i]==None:
+                    continue
+                else:
+                    sheet.row_dimensions[i].height=size[0][i]
+            else:
+                sheet.row_dimensions[i].height=size[0]
+    if size[1]!=None:
+        for i in range(n1):
+            if isinstance(size[1],(tuple,list)):
+                if size[1][i]==None:
+                    continue
+                else:
+                    sheet.column_dimensions[ab_order[i]].width=size[1][i]
+            else:
+                sheet.column_dimensions[ab_order[i]].width=size[1]
+
 '''
 data in the .xlsx is recorded in a dictionary.
 structure of dictionary------------------------------------------------
@@ -8,6 +41,9 @@ value : the list of data
 parameter--------------------------------------------------------------
 container : use list/tuple to store the data
 by_row : list in value's list is recorded in the row of .xlsx file(if True)
+size : size of unit cell
+    size={sheetname:(widths,heights)}
+        widths, heights could be a number or a list/tuple
 '''
 
 def read_xlsx(file,container=list,by_row=True):
@@ -33,7 +69,7 @@ def read_xlsx(file,container=list,by_row=True):
                                      for j in range(n2))
     return d
 
-def write_xlsx(d,output,by_row=True):
+def write_xlsx(d,output,by_row=True,size=None):
     workbook=openpyxl.Workbook()
     sheets=[]
     names=list(d.keys())
@@ -52,10 +88,22 @@ def write_xlsx(d,output,by_row=True):
                 n2=len(lis[i])
                 for j in range(n2):
                     sheets[-1].cell(i+1,j+1,lis[i][j])
+            # manage size
+            if size==None:
+                continue
+            else:
+                if sheet in size.keys():
+                    manage_size(sheets[-1],size[sheet],n1,max(len(j) for j in lis))
         else:
             n1=len(d[sheet])
             for i in range(n1):
                 n2=len(lis[i])
                 for j in range(n2):
                     sheets[-1].cell(j+1,i+1,lis[i][j])
+            # manage size
+            if size==None:
+                continue
+            else:
+                if sheet in size.keys():
+                    manage_size(sheets[-1],size[sheet],max(len(j) for j in lis),n1)
     workbook.save(filename=output)
